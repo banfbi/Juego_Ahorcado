f11174331c9b8f4c88d7caf277e2bf19a9d40579
brundin <134782358+banfbi@users.noreply.github.com>
2024-05-07 20:36:29 +0200

    Update script.js
    Update palabras of "script.js" file.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 /*=======================================================
 Copyright (c) 2024. Alejandro Alberto Jiménez Brundin
 =======================================================*/
-let palabras = [["atlantico", "Un océano"], ["ordenador", "Una máquina"], ["laurel", "Un árbol"], ["plaza", "Espacio público"], ["rueda", "Gran invento"], ["cereza", "Una fruta"], ["petanca", "Un juego"], ["higuera", "Un árbol"], ["everest", "Un monte"], ["relampago", "Antecede al trueno"], ["jirafa", "Un animal"], ["luxemburgo", "Un país"], ["uruguay", "Un país"], ["ilustracion", "Representación gráfica"], ["excursion", "Actividad en la naturaleza"], ["empanadilla", "De la panadería"], ["pastel", "De la pastelería"], ["colegio", "Lugar para estudiar"], ["carrera", "Competición"], ["mermelada", "Confitura"]];
+let palabras = [["mediterraneo", "Un mar"], ["movil", "Un dispositivo"], ["rosa", "Una flor"], ["ciudad", "Alicante"], ["rueda", "Invento revolucionario"], ["manzana", "Una fruta"], ["futbol", "Un juego"], ["ford", "Marca de coche"], ["everest", "Una montaña"], ["relampago", "Parecido al trueno"], ["perro", "Un animal"], ["españa", "Un país"], ["coche", "Un tipo de vehiculo"], ["mercadona", "Supermercado"], ["helado", "Postre"], ["pizza", "Comida restaurante"], ["pastel", "De la pastelería"], ["biblioteca", "Lugar para estudiar"], ["motogp", "Competición"], ["mostaza", "Tipo salsa"]];
 let palabra = "";
 let rand;
 let oculta = [];
